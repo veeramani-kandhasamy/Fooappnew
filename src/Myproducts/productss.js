b58e1260dacdc293eea9f8065665bda6465098dd
veera833 <mrveerakandhasamy@gmail.com>
2024-06-29 14:29:32 +0530

    first commit

---
+++ src/Myproducts/productss.js
@@ -0,0 +1,183 @@
+export default [
+    {
+      "id": 1,
+      "name": "Redgrill",
+      "description": "Very Spicy and juicy grill dish",
+      "price": 320,
+      "image": require("../assets/spicicy red.jpg"),
+      "restaurant": "BarbeQueen",
+      "quantity": 1
+    },
+    {
+      "id": 2,
+      "name": "Andra style grill",
+      "description": "Andra style popular south indian dish",
+      "price": 280,
+      "image": require("../assets/ChickenGheeRoast.jpg"),
+      "restaurant": "BarbeQueen",
+      "quantity": 1
+    },
+    {
+      "id": 3,
+      "name": "Muttonnally",
+      "description": "Athentic mutton nalli kulambu",
+      "price": 250,
+      "image": require("../assets/muttongravy.jpg"),
+      "restaurant": "BarbeQueen",
+      "quantity": 1
+    },
+    {
+      "id": 4,
+      "name": "NattuKolzhiPepper",
+      "description": "Nattu kolzhi pepper  is a popular south indian dish",
+      "price": 350,
+      "image": require("../assets/nattu-kozhi-pepper-roast.jpg"),
+      "restaurant": "BarbeQueen",
+      "quantity": 1
+    },
+    {
+      "id": 5,
+      "name": "kothuIdiyapam",
+      "description": "different pepper added kothu idiyappam",
+      "price": 130,
+      "image": require("../assets/egg-kothu-idiyappam.jpg"),
+      "restaurant": "BarbeQueen",
+      "quantity": 1
+    },
+    {
+      "id": 6,
+      "name": "Fish fry",
+      "description": "South side delicious fish fry",
+      "price": 140,
+      "image": require("../assets/fishfry.jpg"),
+      "restaurant": "BarbeQueen",
+      "quantity": 1
+    },
+    {
+      "id": 7,
+      "name": "panner Gravy",
+      "description": "Panner specialised dish spicily",
+      "price": 240,
+      "image": require("../assets/kadai-paneer.jpg"),
+      "restaurant": "BarbeQueen",
+      "quantity": 1
+    },
+    {
+      "id": 8,
+      "name": "Hydrabadi Briyani",
+      "description": "A tremendous dum and juicy Briyani",
+      "price": 260,
+      "image": require("../assets/briyani.jpg"),
+      "restaurant": "BarbeQueen",
+      "quantity": 1
+    },
+    {
+      "id": 9,
+      "name": "Butter Naan",
+      "description": "Butter and oil stuffed naan",
+      "price": 40,
+      "image": require("../assets/gheenaan.jpg"),
+      "restaurant": "BarbeQueen",
+      "quantity": 1
+    },
+    {
+      "id": 10,
+      "name": "Egg Pupa",
+      "description": "Arabic style pepper added eggpupa",
+      "price": 80,
+      "image": require("../assets/eggpupa.jpg"),
+      "restaurant": "BarbeQueen",
+      "quantity": 1
+    },
+    {
+      "id": 11,
+      "name": "Mutton Briyani",
+      "description": "Mutton flembed Juicy Briyani",
+      "price": 280,
+      "image": require("../assets/briyani1.jpg"),
+      "restaurant": "BarbeQueen",
+      "quantity": 1
+    },
+    {
+      "id": 12,
+      "name": "Chicken Karry Dosai",
+      "description": "Chicken fried Doasa with Gravy",
+      "price": 220,
+      "image": require("../assets/chicken-kari-dosai-1.jpg"),
+      "restaurant": "BarbeQueen",
+      "quantity": 1
+    },
+    {
+      "id": 13,
+      "name": "Chicken KFC",
+      "description": "KFC crispy chicken legs",
+      "price": 190,
+      "image": require("../assets/kfc.jpg"),
+      "restaurant": "BarbeQueen",
+      "quantity": 1
+    },
+    {
+      "id": 14,
+      "name": "Chilli Prawn",
+      "description": "Chilli added crispy Prawn",
+      "price": 224,
+      "image": require("../assets/chilli-prawns.jpg"),
+      "restaurant": "Bar",
+      "quantity": 1},
+
+
+      {
+        "id": 15,
+        "name": "Black Pepper chicken",
+        "description": "Natu Kozhi Pepper added chicken fry",
+        "price": 294,
+        "image": require("../assets/black-pepper-chicken-boneless.jpg"),
+        "restaurant": "BarbeQueen",
+        "quantity": 1
+      },
+      {
+        "id": 16,
+        "name": "chilli Parato",
+        "description": "Chiili and  Pepper added Parota fry",
+        "price": 214,
+        "image": require("../assets/chilliparota.jpg"),
+        "restaurant": "BarbeQueen",
+        "quantity": 1
+      },
+      {
+        "id": 17,
+        "name": "Black Welvet",
+        "description": "Pieced Cakes Of Black chopped Welvet",
+        "price": 115,
+        "image": require("../assets/moru-moru-stick-chicken.jpg"),
+        "restaurant": "BarbeQueen",
+        "quantity": 1
+      },
+      {
+        "id": 18,
+        "name": "Egg parota",
+        "description": "specialised Bread with egg Omlete",
+        "price": 134,
+        "image": require("../assets/ceylon-egg-parotta-1.jpg"),
+        "restaurant": "BarbeQueen",
+        "quantity": 1
+      },
+      {
+        "id": 19,
+        "name": "Billa Grill",
+        "description": "Brand new Coca-cola -cooled",
+        "price": 379,
+        "image": require("../assets/alfaham-barbeque.jpg"),
+        "restaurant": "BarbeQueen",
+        "quantity": 1
+      },
+      {
+        "id": 20,
+        "name": "samosa",
+        "description": "Jackfruit and water Melon Mixed Juice",
+        "price": 53,
+        "image": require("../assets/samosa (1).jpg"),
+        "restaurant": "BarbeQueen",
+        "quantity": 1
+      }
+    ];
